20c5e47720063f500bda05198a280982ff1ef2f0
Franz Bendezu <franzant20@gmail.com>
2024-06-01 22:12:13 -0500

    feat: agregar estado de carga y error

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,13 +4,26 @@ const API_URL = "http://localhost:8080";
 
 export function useFetch(path, options) {
   const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(API_URL + path, options);
-      const data = await response.json();
-      setData(data);
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${API_URL}${path}`, options);
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchData();
   }, []);
-  return { data };
+  return {
+    data,
+    isLoading,
+    error,
+  };
 }

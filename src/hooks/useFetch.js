f5f6c9374d7d53336c2a5930a5459a1ea5c64b75
Franz Bendezu <franzant20@gmail.com>
2024-06-01 23:08:25 -0500

    feat: exportar mutate en use fetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,25 +6,26 @@ export function useFetch(path, options) {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsLoading(true);
-        const response = await fetch(`${API_URL}${path}`, options);
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
+  async function mutate() {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${API_URL}${path}`, options);
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
     }
-    fetchData();
+  }
+  useEffect(() => {
+    mutate();
   }, []);
   return {
     data,
     isLoading,
     error,
+    mutate
   };
 }
 
